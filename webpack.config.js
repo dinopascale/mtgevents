1e64b2f92d3d6198e77c784242336becf9bcc2ae
dinopascale <dino.pascale@gmail.com>
2017-10-03 19:58:02 +0200

    Mtg Events App

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: "./app/entry.js",
+  output: {
+    path: __dirname+"/app/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
